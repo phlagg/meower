66c1eb8e7ebfaed3e02b59cd7424a70b9de5edc3
Michael <michaelthomaswoods@gmail.com>
2018-10-23 22:18:24 -0700

    Version 0.1

--- client/client.js
+++ client/client.js
@@ -1,7 +1,10 @@
 const form = document.querySelector('form');
 const loadingElement = document.querySelector('.loading');
+const mewsElement = document.querySelector('.mews')
+const API_URL = 'http://localhost:5000/mews';
+loadingElement.style.display = '';
 
-loadingElement.style.display = 'none';
+listAllMews();
 
 form.addEventListener('submit', (event) => {
 event.preventDefault();
@@ -14,8 +17,53 @@ const mew = {
     name,
     content
 };
-    console.log(mew);
+   
     form.style.display = 'none';
     loadingElement.style.display = '';      
     
+    fetch(API_URL, {
+        method: 'POST',
+        body: JSON.stringify(mew), 
+        headers: {
+            'content-type': 'application/json'
+        }
+    }).then(response => response.json())
+        .then(createdMew => {
+            console.log(createdMew);
+            form.reset();
+            form.style.display = '';
+            listAllMews();
+            loadingElement.style.display = 'none'; 
+            
+    });
+
 });
+
+
+function listAllMews(){
+    fetch(API_URL)
+        .then (response => response.json())
+        .then(mews => {
+            mewsElement.innerHTML = '';
+            mews.reverse();
+            mews.forEach(mew => {
+                const div = document.createElement('div');
+
+                const header = document.createElement('h3');
+                header.textContent = mew.name;
+                const contents = document.createElement('p');
+                contents.textContent = mew.content;
+                
+                const date = document.createElement('small');
+                date.textContent = new Date(mew.created);
+
+                div.appendChild(header);
+                div.appendChild(contents);
+                div.appendChild(date);
+
+                mewsElement.appendChild(div);
+            });
+        });
+        loadingElement.style.display = 'none'; 
+
+};
